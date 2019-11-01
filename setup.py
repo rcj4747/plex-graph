e22ee09bf52da3bb192cbcecca93a9b22a1f80b7
Robert C Jennings <robert.jennings@canonical.com>
2019-11-01 11:32:51 -0500

    Add package skeleton

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+Graph relationships between movies on a plex server
+"""
+import os
+
+from setuptools import find_packages, setup
+
+reqs_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+
+with open(reqs_path, 'r') as req_file:
+    dependencies = req_file.readlines()
+
+setup(
+    name='plex_graph',
+    version='0.1.0',
+    license='GPLv3',
+    author='Robert C Jennings',
+    author_email='rcj4747@gmail.com',
+    description='Graph relationships between movies on a plex server',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'plex-graph = plex_graph.cli:main',
+        ],
+    },
+)
