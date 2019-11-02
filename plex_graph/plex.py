0490fbaf6e764630de236aa379fc52e43361af31
Robert C Jennings <robert.jennings@canonical.com>
2019-11-02 13:26:35 -0500

    Add mypy to pre-commit and update typing

--- plex_graph/plex.py
+++ plex_graph/plex.py
@@ -7,18 +7,29 @@ import ast
 import shelve
 from configparser import ConfigParser
 from dataclasses import dataclass, field
-from typing import List, Set, Tuple
+from typing import List, Optional, Set, Tuple
 
 import matplotlib.pyplot as plot
 import networkx as nx
 import requests
-from plexapi.myplex import MyPlexAccount
+from plexapi.library import LibrarySection
+from plexapi.myplex import MyPlexAccount, MyPlexResource
 from plexapi.server import PlexServer
 
 MIN_RELATIONS: int = 11
 PLEX_USER: str = ''
 PLEX_PASS: str = ''
 CONFIG_FILE: str = 'config.ini'
+
+
+@dataclass()
+class PlexServerConfig:
+    '''PlexServer connection data'''
+    name: str
+    urls: List[str]
+    token: str
+    lasturl: Optional[str] = None
+    connection: Optional[PlexServer] = None
 
 
 @dataclass(eq=True, frozen=True)
@@ -29,15 +40,15 @@ class Movie:
     studio: str
     content_rating: str
     rating: str
-    writers: Tuple[str] = field(default_factory=tuple)
-    directors: Tuple[str] = field(default_factory=tuple)
-    actors: Tuple[str] = field(default_factory=tuple)
-    genres: Tuple[str] = field(default_factory=tuple)
+    writers: Tuple = field(default_factory=tuple)
+    directors: Tuple = field(default_factory=tuple)
+    actors: Tuple = field(default_factory=tuple)
+    genres: Tuple = field(default_factory=tuple)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.name}'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.name}'
 
 
@@ -55,8 +66,9 @@ def write_config(config: ConfigParser) -> None:
 def generate_config(user: str, password: str) -> None:
     '''Build the ConfigParser data for plex server authentication'''
     config: ConfigParser = ConfigParser()
-    account = MyPlexAccount(user, password)
+    account: MyPlexAccount = MyPlexAccount(user, password)
 
+    servers: List[MyPlexResource]
     servers = [resource for resource in account.resources()
                if 'server' in resource.provides.split(',')]
     for server in servers:
@@ -67,69 +79,68 @@ def generate_config(user: str, password: str) -> None:
     write_config(config)
 
 
-def read_config():
-    '''Read plex server authentication from disk. Returns ConfigParser obj'''
+def read_config() -> ConfigParser:
+    '''Read plex server authentication from disk.'''
     with open(CONFIG_FILE, 'r') as config_file:
         config = ConfigParser()
         config.read_file(config_file)
     return config
 
 
-def get_servers(config):
-    '''Connect to plex servers from our config and return the server objs'''
-    servers = []
+def get_servers(config: ConfigParser) -> List[PlexServerConfig]:
+    '''Connect to plex servers from our config'''
+    servers: List[PlexServerConfig] = []
     for key in list(config.keys()):
         if key.startswith('server:'):
-            server = key.split(':')[1]
-            urls = ast.literal_eval(config[key]['baseurls'])
-            server = {'name': server,
-                      'urls': urls,
-                      'token': config[key]['token'],
-                      }
+            name = key.split(':')[1]
+            urls: List[str] = ast.literal_eval(config[key]['baseurls'])
+            server = PlexServerConfig(name=name, urls=urls,
+                                      token=config[key]['token'])
             if 'lasturl' in config[key]:
-                server['lasturl'] = config[key]['lasturl']
+                server.lasturl = config[key]['lasturl']
             servers.append(server)
 
     for server in servers:
-        print(f'Connecting to server {server["name"]}')
+        print(f'Connecting to server {server.name}')
         connection = None
 
         # See if the last used URL works
-        if 'lasturl' in server:
+        if server.lasturl:
             try:
-                print(f' Trying {server["lasturl"]} ... ', end='')
-                connection = PlexServer(baseurl=server['lasturl'],
-                                        token=server['token'],
+                print(f' Trying {server.lasturl} ... ', end='')
+                connection = PlexServer(baseurl=server.lasturl,
+                                        token=server.token,
                                         timeout=5)
                 print('connected')
-                server['connection'] = connection
+                server.connection = connection
                 continue
             except requests.exceptions.ConnectTimeout:
                 print('no connection')
 
         # Try connecting to all possible URLs
-        for url in server['urls']:
+        for url in server.urls:
             # Skip retrying lasturl
-            if 'lasturl' in server:
-                if url == server['lasturl']:
+            if server.lasturl:
+                if url == server.lasturl:
                     continue
             try:
                 print(f' Trying {url} ... ', end='')
                 connection = PlexServer(baseurl=url,
-                                        token=server['token'],
+                                        token=server.token,
                                         timeout=5)
                 print('connected')
-                server['lasturl'] = url
-                server['connection'] = connection
+                server.lasturl = url
+                server.connection = connection
                 break
             except requests.exceptions.ConnectTimeout:
                 print('no connection')
         if not connection:
-            print(f'No connection could be made to {server["name"]}')
+            print(f'No connection could be made to {server.name}')
     return servers
 
 
-def update_config(config, servers):
+def update_config(config: ConfigParser,
+                  servers: List[PlexServerConfig]) -> ConfigParser:
     '''
     After connecting to servers, update ConfigParser data with lasturl data.
 
@@ -140,13 +151,13 @@ def update_config(config, servers):
     '''
 
     for server in servers:
-        if 'lasturl' in server:
-            config[f'server:{server["name"]}']['lasturl'] = server['lasturl']
+        if server.lasturl:
+            config[f'server:{server.name}']['lasturl'] = server.lasturl
     write_config(config)
     return config
 
 
-def get_plex_movie_sections(server):
+def get_plex_movie_sections(server: PlexServer) -> List[LibrarySection]:
     '''
     Finds all 'Movie' library sections on a server.
 
@@ -159,7 +170,7 @@ def get_plex_movie_sections(server):
     return movie_sections
 
 
-def parse_plex_movies(movie_sections):
+def parse_plex_movies(movie_sections: List[LibrarySection]) -> None:
     '''Parse all movies in the list of movie library sections.
 
     Populate the global list of movies, genres, and people from Plex
@@ -175,22 +186,22 @@ def parse_plex_movies(movie_sections):
             print(f'Processing({movie_count}/{movie_total}) "{media.title}"')
             movie_count += 1
             media.reload()  # Get full object metadata
-            writers: List[str] = set()
+            writers: Set[str] = set()
             for person in media.writers:
                 person = person.tag
                 PEOPLE.add(person)
                 writers.add(person)
-            directors: List[str] = set()
+            directors: Set[str] = set()
             for person in media.directors:
                 person = person.tag
                 PEOPLE.add(person)
                 directors.add(person)
-            actors: List[str] = set()
+            actors: Set[str] = set()
             for person in media.roles:
                 person = person.tag
                 PEOPLE.add(person)
                 actors.add(person)
-            genres: List[str] = set()
+            genres: Set[str] = set()
             for genre in media.genres:
                 genre = genre.tag
                 GENRES.add(genre)
@@ -207,7 +218,7 @@ def parse_plex_movies(movie_sections):
                                 ))
 
 
-def write_shelve():
+def write_shelve() -> None:
     '''Store the global list of Movies, Genres, and People on disk
 
     To avoid pulling data from the plex server on each run we write
@@ -220,7 +231,7 @@ def write_shelve():
         data['PEOPLE'] = PEOPLE
 
 
-def read_shelve():
+def read_shelve() -> None:
     '''Read the global list of Movies, Genres, and People from disk
 
     To avoid pulling data from the plex server on each run we use
@@ -236,7 +247,7 @@ def read_shelve():
         PEOPLE = data['PEOPLE']
 
 
-def generate_data():
+def generate_data() -> None:
     '''Glue code incorporating all the functions to read data from plex
     and generate our data for future runs.
     '''
@@ -246,13 +257,13 @@ def generate_data():
     servers = get_servers(config)
     config = update_config(config, servers)
     for server in servers:
-        print(f'Indexing server {server["name"]}')
+        print(f'Indexing server {server.name}')
         movie_sections = get_plex_movie_sections(server)
         parse_plex_movies(movie_sections)
     write_shelve()
 
 
-def graph_data():
+def graph_data() -> None:
     '''Experiment with graphing Movie and Actor relationships'''
     print('Loading data from disk...', end='')
     read_shelve()
